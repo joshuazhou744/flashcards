a46e128d12e045c85581a0229883ea02f7c0f74b
Joshua Zhou <j.zhou11@share.epsb.ca>
2024-09-20 12:43:09 -0400

    Add next button

--- frontend/src/components/FlashcardList.tsx
+++ frontend/src/components/FlashcardList.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import Flashcard from "./Flashcard";
+import './FlashcardList.css'
 
 interface Flashcard {
     id: number;
@@ -13,11 +15,21 @@ interface Flashcard {
 
 
 export default function FlashcardList({ flashcards }: FlashcardListProps) {
+  const [current, setCurrent] = useState(0);
+
+  const nextCard = () => {
+    setCurrent((prev) => 
+      prev === flashcards.length - 1 ? 0 : prev + 1
+    )
+  }
+
   return (
-    <div className="card-grid">
-      {flashcards.map(flashcard => {
-        return <Flashcard flashcard={flashcard} key={flashcard.id} />
-      })}
+    <div className="flashcard-container">
+      <div className="flashcard">
+        <Flashcard flashcard={flashcards[current]} key={flashcards[current].id} />
+      </div>
+      <button className="nextbutton" onClick={nextCard}>Next Flashcard</button>
+      
     </div>
   )
 }
