edce63015c1e1777b09797fe8cf61086ada65016
Joshua Zhou <j.zhou11@share.epsb.ca>
2024-09-24 12:38:09 -0400

    Cleanup gui and implement create deck in frontend

--- frontend/src/components/Decklist.tsx
+++ frontend/src/components/Decklist.tsx
@@ -8,17 +8,50 @@ const url = "http://localhost:8000/api"
 export default function Decklist() {
 
   const [decks, setDecks] = useState<[]>([])
+  const [newDeck, setNewDeck] = useState<string>("")
+  const [invalid, setInvalid] = useState<boolean>(false)
 
+  // fetch decks on start
   useEffect(() => {
     axios.get(`${url}/get_decks`)
     .then(res => {
       setDecks(res.data.decks)
     })
-  }, [decks])
+  }, [newDeck])
+
+  const handleCreate = () => {
+    if (newDeck.trim() !== "") {
+      axios.post(`${url}/create_deck/${newDeck}`)
+        .then(res => {
+          setNewDeck("")
+          setInvalid(false)
+        })
+        .catch(error => {
+          console.error(error)
+        })
+    } else {
+      setInvalid(true)
+    }
+  }
 
   return (
     <div className="main">
-      <h2 className="title">Decks</h2>
+      <h1 className="title">Decks</h1>
+      <div className="create-deck">
+        <input 
+          type="text"
+          value={newDeck}
+          onChange={(e) => {
+            setNewDeck(e.target.value)
+            if (e.target.value.trim() !== "") {
+              setInvalid(false)
+            }
+          }}
+          placeholder="New Deck Name"
+        />
+        <button onClick={handleCreate} disabled={invalid || newDeck.trim() === ""}>Create Deck</button>
+      </div>
+
       <div className="decks-container">
         {decks.length > 0 && decks.map((deck) => (
             <Link className="card" to={`/deck/${deck}`} key={deck}>
